724b4b2c063915231edfafc37c2496f02f56b082
dstevdev <dstev.dev@gmail.com>
2025-12-29 09:34:34 -0500

    - Removed the CSS files and introduced Mantine with a base to get begin functions.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Button,
+  Input,
+  Container,
+  AppShell,
+  Title,
+  Center,
+  Stack,
+  Group,
+} from "@mantine/core";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppShell padding="md" header={{ height: 60 }}>
+        <AppShell.Header>Test</AppShell.Header>
+        <AppShell.Main>
+          <Center maw="100%" h="80vh">
+            <Stack align="center">
+              <Title order={1}>LinkChop</Title>
+              <Group align="center">
+                <Input placeholder="URL to Chop" size="lg" w={500} />
+                <Button size="lg">Chop</Button>
+              </Group>
+            </Stack>
+          </Center>
+        </AppShell.Main>
+        <AppShell.Footer>Footer</AppShell.Footer>
+      </AppShell>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  </StrictMode>
+);
